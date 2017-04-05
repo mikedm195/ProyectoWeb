9e25b497139d2418062900aac72f00f416da01c4
sbaidachni <mikedm195@gmail.com>
2017-04-04 20:05:34 -0500

    .

--- app/controllers/users.js
+++ app/controllers/users.js
@@ -1,29 +1,33 @@
 var users = require('../modules/users')
+var fs = require('fs');
+var bf = require('../../public/javascripts/bloomfilter');
 
-module.exports.index = function(req, res){
-	if(req.session){
+module.exports.index = function (req, res) {
+	if (req.session) {
 		req.session = null;
-		res.render('login',{mensaje:''});
-	}else{
-		res.render('login',{mensaje:''});
+		res.render('login', { mensaje: '' });
+	} else {
+		res.render('login', { mensaje: '' });
 	}
 };
 
-module.exports.login = function(req, res){
+module.exports.login = function (req, res) {
+
+	//verify
 	users.getUser(req.body.user, req.body.password,
-		function(err,users){
+		function (err, users) {
 			console.log("users: ", users)
-			if (err){
+			if (err) {
 				console.log(err);
 			}
-			else if(users.status.localeCompare('ERROR') == 0){
+			else if (users.status.localeCompare('ERROR') == 0) {
 				console.log("Error");
-				res.render('login',{mensaje:'Error al iniciar sesion'});
+				res.render('login', { mensaje: 'Error al iniciar sesion' });
 			}
-			else{
+			else {
 				req.session = {
-				  name: users.user,
-				  keys: ['key1', 'key2']
+					name: users.user,
+					keys: ['key1', 'key2']
 				}
 				res.json(users);
 				//res.send(JSON.stringify(users));
@@ -32,18 +36,18 @@ module.exports.login = function(req, res){
 	);
 };
 
-module.exports.editarUsuario = function(req, res){
+module.exports.editarUsuario = function (req, res) {
 	users.getUserByName(req.session.name,
-		function(err,usuarios){			
-			if (err){
+		function (err, usuarios) {
+			if (err) {
 				console.log(err);
 			}
-			else if(usuarios.status.localeCompare('ERROR') == 0){
+			else if (usuarios.status.localeCompare('ERROR') == 0) {
 				console.log("Error");
 			}
-			else{
+			else {
 				console.log(usuarios.data);
-				res.render('editarUsuario',{usuarios:usuarios.user});
+				res.render('editarUsuario', { usuarios: usuarios.user });
 				// res.json(series);
 			}
 		}
@@ -51,40 +55,59 @@ module.exports.editarUsuario = function(req, res){
 	// res.render('index', { title: 'Express' });
 };
 
-module.exports.agregarUsuario = function(req, res){
+module.exports.agregarUsuario = function (req, res) {
 	res.render('agregarUsuario');
 };
 
-module.exports.post = function(req, res){
+module.exports.post = function (req, res) {
+	var bloom = new bf.BloomFilter(
+		10 * 54000, // number of bits to allocate.
+		8        // number of hash functions.
+	);
+	console.log("hola");
+	fs.readFile('../../public/javascripts/human-ans-shorter.txt', 'utf8', function (err, data) {
+		if (err) {
+			return console.log(err);
+		}
+		var s = data.split('\n');
+		s.forEach(function (pwd) {
+			bloom.add(pwd);
+		});
+		var array = [].slice.call(bloom.buckets),
+			json = JSON.stringify(array);
+		fs.writeFile('../../public/javascripts/bloomdata_short_pwd.js', json);
+	});
+
+	//   res.json("alls well that ends well :P");
 	users.postUsers(req.body,
-		function(err,usuarios){
-			if (err){
+		function (err, usuarios) {
+			if (err) {
 				console.log(err);
-			}else{
+			} else {
 				res.json(usuarios);
 			}
 		}
 	);
 };
 
-module.exports.put = function(req, res){
+module.exports.put = function (req, res) {
 	users.putUsers(req.body,
-		function(err,usuarios){
-			if (err){
+		function (err, usuarios) {
+			if (err) {
 				console.log(err);
-			}else{
+			} else {
 				res.json(usuarios);
 			}
 		}
 	);
 };
 
-module.exports.delete = function(req, res){
+module.exports.delete = function (req, res) {
 	users.deleteUsers(req.body,
-		function(err,usuarios){
-			if (err){
+		function (err, usuarios) {
+			if (err) {
 				console.log(err);
-			}else{
+			} else {
 				res.json(usuarios);
 			}
 		}
